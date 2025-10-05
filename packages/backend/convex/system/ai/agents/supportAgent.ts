c5e4347a7a4919d460986ea2d45da0564e9d1bc1
Syed Ahmad Alisha <ahmadalishasyed@gmail.com>
2025-10-05 10:02:26 +0530

    22 : ai search tool

--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -1,9 +1,9 @@
 import { components } from "../../../_generated/api";
 import { Agent } from "@convex-dev/agent";
-import { openai } from "@ai-sdk/openai";
 import { google } from '@ai-sdk/google';
+import { SUPPORT_AGENT_PROMPT } from "../constants";
 
 export const supportAgent = new Agent(components.agent,{
-    chat: google.chat("gemini-1.5-flash"),
-    instructions: `you are a customer support agent. Use "resolveConversation" tool when user express finalization of the conversation. Use "escalateConversation" tool when user expresses frustation , or request a human support or human explicitly`, 
+    chat: google.chat("gemini-2.5-flash"),
+    instructions: SUPPORT_AGENT_PROMPT, 
 })
